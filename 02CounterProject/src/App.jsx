ed97fbbec40ce8a8370491514dd725ae24ccabdc
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-08-25 22:12:13 +0530

    interviw tip

--- 02CounterProject/src/App.jsx
+++ 02CounterProject/src/App.jsx
@@ -6,17 +6,27 @@ import './App.css'
 
 function App() {
   // hooks usage 
-  let [counter, setCounter ]=useState(0)
- 
+  let [counter, setCounter] = useState(0)
+
   //let counter =0
-  const addvalue= ()=>{
+  const addvalue = () => {
     console.log(" Add Value Clicked");
     counter= counter+1;
     setCounter(counter)
+    /*setCounter(counter + 1)
+    setCounter(counter + 1)
+    setCounter(counter + 1)
+    setCounter(counter + 1)*/
+    //  the  above is a confusion  that if above commented setcouter methods will execute in batch and gives  solution directly simply 
+    // Ones without  altering the expected  output and its because of the use state->   
+    // we can execute the task of increament the output in real by another method-> 
+    /*setCounter(prevcounter=>prevcounter + 1)
+    setCounter(prevcounter=>prevcounter + 1)
+    setCounter(prevcounter=>prevcounter + 1)*/
   }
-  const removevalue=()=>{
-      console.log(" Delete Value Clicked");
-    counter= counter-1;
+  const removevalue = () => {
+    console.log(" Delete Value Clicked");
+    counter = counter - 1;
     setCounter(counter)
   }
   return (
@@ -25,7 +35,7 @@ function App() {
       <h2>Counter  value: {counter}</h2>
 
       <button onClick={addvalue}>Add value</button>
-      <br/>
+      <br />
       <button onClick={removevalue}> Delete Value</button>
     </>
   )
