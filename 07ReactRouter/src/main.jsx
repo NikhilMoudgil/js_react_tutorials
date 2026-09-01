aae85186c73ea47a336fe359c1ca458329ce9bde
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-01 23:41:14 +0530

    learning about react  router library and use react router in project

---
+++ 07ReactRouter/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/home/home.jsx'
+import Layout from './layout.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {path:"",element:<Home/>},
+      {path:"about",element:<About/>},
+      {path:"contact",element:<Contact/>}
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
